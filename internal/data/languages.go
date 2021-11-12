453f7254ea4b08ab06759c37c2b2ffc0c14aa3bc
Hugh Do <mhughdo@gmail.com>
2021-11-12 15:52:58 +0700

    GET /v1/langugae/:id API (#3)
    * GET /v1/langugae/:id API

--- internal/data/languages.go
+++ internal/data/languages.go
@@ -3,6 +3,7 @@ package data
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"scv/models"
 )
 
@@ -14,4 +15,18 @@ type Language models.Language
 
 func (l LanguageModel) GetAll() ([]*models.Language, error) {
 	return models.Languages().All(context.Background(), l.DB)
+}
+
+func (l LanguageModel) Get(id int) (*models.Language, error) {
+	language, err := models.FindLanguage(context.Background(), l.DB, id)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return language, nil
 }
